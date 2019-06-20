c58612bf462bbe8cfafb1e4b5db6e316de48701f
Wojciech Nowak <cartoonmail20gmail.com>
2019-06-20 21:15:41 +0200

    Initial commit - implemented preconditions

---
+++ src/main/java/com/wnowakcraft/preconditions/Preconditions.java
@@ -0,0 +1,48 @@
+package com.wnowakcraft.preconditions;
+
+import java.util.Collection;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+public class Preconditions {
+    private Preconditions() {
+    }
+
+    public static <T> T requireNonNull(T argument, String argumentName) {
+        return Objects.requireNonNull(argument, message(argumentName, "== null"));
+    }
+
+    public static <T extends CharSequence> T requireNonEmpty(T charSequence, String argumentName) {
+        requireNonNull(charSequence, argumentName);
+        requireThat(charSequence.length() > 0, message(argumentName, "needs to be specified"));
+        return charSequence;
+    }
+
+    public static <T> Collection<T> requireNonEmpty(Collection<T> collection, String argumentName) {
+        requireNonNull(collection, argumentName);
+        requireThat(collection.size() > 0, message(argumentName, "needs to be specified"));
+        return collection;
+    }
+
+    public static void requireStateThat(boolean condition, String violationMessage) {
+        if (!condition) {
+            throw new IllegalStateException(violationMessage);
+        }
+    }
+
+    public static void requireThat(boolean condition, String violationMessage) {
+        if (!condition) {
+            throw new IllegalArgumentException(violationMessage);
+        }
+    }
+
+    public static void requireThat(boolean condition, Supplier<RuntimeException> conditionFailedExceptionProvider) {
+        if (!condition) {
+            throw conditionFailedExceptionProvider.get();
+        }
+    }
+
+    private static String message(String argumentName, String violationMessage) {
+        return argumentName + " " + violationMessage;
+    }
+}
